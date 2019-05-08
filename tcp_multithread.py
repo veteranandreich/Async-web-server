13cee30563cbec5c1ce63d2c2550d1d67927c61d
Vladimir <vladimir27goncharov@icloud.com>
2019-05-08 17:19:06 +0300

    Process pool with threads added

--- tcp_multithread.py
+++ tcp_multithread.py
@@ -1,57 +1,67 @@
 import socket
 import threading
+import multiprocessing
+import time
 import logging
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format='[%(levelname)s] (%(threadName)-10s) %(message)s'
+    format='[%(levelname)s] (%(processName)-10s) (%(threadName)-10s) %(message)s'
 )
 
-
-def client_handler(sock: socket.socket, address: str, port: int) -> None:
+def worker_thread(serversocket):
     while True:
-        try:
-            message = sock.recv(1024)
-            logging.debug(f"Recv: {message} from {address}:{port}")
-        except OSError:
-            break
+        clientsocket, (client_address, client_port) = serversocket.accept()
+        logging.debug(f"New client {client_address}:{client_port}")
 
-        if len(message) == 0:
-            break
-
-        sent_message = message
         while True:
-            sent_len = sock.send(sent_message)
-            if sent_len == len(sent_message):
+            try:
+                data = clientsocket.recv(1024)
+                logging.debug(f"Recv: {data} from {client_address}:{client_port}")
+            except OSError:
                 break
-            sent_message = sent_message[sent_len:]
-        logging.debug(f"Send: {message} to {address}:{port}")
-    sock.close()
-    logging.debug(f"Bye-bye: {address}:{port}")
 
+            if len(data) == 0:
+                break
 
-def main(host: str = 'localhost', port: int = 9090) -> None:
+            sent_data = data
+            while True:
+                sent_len = clientsocket.send(data)
+                if sent_len == len(data):
+                    break
+                sent_data = sent_data[sent_len:]
+            logging.debug(f"Send: {data} to {client_address}:{client_port}")
+
+        clientsocket.close()
+        logging.debug(f"Bye-bye: {client_address}:{client_port}")
+
+def worker_process(serversocket):
+    NUMBER_OF_THREADS = 10
+    for _ in range(NUMBER_OF_THREADS):
+        thread = threading.Thread(target=worker_thread,
+            args=(serversocket,))
+        thread.daemon = True
+        thread.start()
+
+    while True:
+        time.sleep(1)
+
+def main(host='localhost', port=9090):
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
     serversocket.bind((host, port))
-    serversocket.listen(128)
-    socket.setdefaulttimeout(10)
+    serversocket.listen(5)
 
-    print(f"Starting TCP Echo Server at {host}:{port}")
-    try:
-        while True:
-            clientsocket, (client_address, client_port) = serversocket.accept()
-            logging.debug(f"New client: {client_address}:{client_port}")
-            client_thread = threading.Thread(
-                target=client_handler,
-                args=(clientsocket, client_address, client_port))
-            client_thread.daemon = True
-            client_thread.start()
-    except KeyboardInterrupt:
-        print("Shutting down")
-    finally:
-        serversocket.close()
+    NUMBER_OF_PROCESS = multiprocessing.cpu_count()
+    logging.debug(f"Number of processes {NUMBER_OF_PROCESS}")
+    for _ in range(NUMBER_OF_PROCESS):
+        process = multiprocessing.Process(target=worker_process,
+            args=(serversocket,))
+        process.daemon = True
+        process.start()
 
+    while True:
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
